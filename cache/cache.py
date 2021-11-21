66777faa88454ec26b78cf231a12b1eb90cbcf67
Umar Khan <umar_khan50@hotmail.com>
2021-11-21 21:48:59 +0000

    Add store

--- cache/cache.py
+++ cache/cache.py
@@ -6,30 +6,30 @@ from cache.store import Store
 class Cache:
     """Base class for caching data."""
 
-    def __init__(self, size, store: Optional[Store] = None):
-        self._store = store or Store()
-        self._size = size
+    def __init__(self, store: Store):
+        self._store = store
 
-    def pre_put_hook(self, key, value):
-        """Overwrite with any pre save actions."""
+    def put_hook(self, key, value):
+        """Overwrite with pre save actions."""
         return key, value
 
-    def post_put_hook(self, key, value):
-        """"Overwrite with any post save actions."""
+    def get_hook(self, key, value):
+        """Overwrite with pre fetch actions."""
+        return key, value
 
     def put(self, key, value):
         """Create or update the value for the given key."""
-        key, value = self.pre_put_hook(key, value)
+        key, value = self.put_hook(key, value)
         self._store[key] = value
-        self.post_put_hook(key, value)
 
     def get(self, key):
         """Fetch the value for the given key from the store."""
-        return self._store.get(key)
+        _, value = self.get_hook(key, self._store[key])
+        return value
 
     def delete(self, key):
         """Delete the data for the given key from the store."""
-        self._store.pop(key)
+        self._store.pop(key, None)
 
 # TODO
 #     - handle error if key is not found in get method.
--- cache/store.py
+++ cache/store.py
@@ -3,4 +3,36 @@ from collections import OrderedDict
 
 
 class Store(OrderedDict):
-    """Base class for data stores."""
+    """A low level cache implementation."""
+
+    def __init__(self, size: int, *args, **kwargs):
+        self.size = size
+        super().__init__(*args, **kwargs)
+
+    def get_overflow(self):
+        """Get the key of the item to remove if there are too many items."""
+        return next(iter(self))
+
+    def add_new_hook(self, key, value):
+        """Run this hook before adding a new item to the store."""
+        return key, value
+
+    def update_hook(self, key, value):
+        """Run this hook before updating an existing store item."""
+        return key, value
+
+    def get_hook(self, _key, value):
+        """Run this hook before fetching an item from the store."""
+        return value
+
+    def __getitem__(self, key):
+        return self.get_hook(key, super().__getitem__(key))
+
+    def __setitem__(self, key, value) -> None:
+        if key in self:
+            key, value = self.update_hook(key, value)
+        else:
+            if len(self) == self.size:
+                del self[self.get_overflow()]
+            key, value = self.add_new_hook(key, value)
+        super().__setitem__(key, value)
