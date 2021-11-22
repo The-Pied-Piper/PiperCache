32f8ce7cd80df596ff62e8dbc0d175d451dde298
Umar Khan <umar_khan50@hotmail.com>
2021-11-22 00:54:41 +0000

    add the setup script.

---
+++ setup.py
@@ -0,0 +1,10 @@
+"""Setup script for the library."""
+from distutils.core import setup
+
+setup(name="Cache",
+    version="1.0",
+    description="A library for building caches in python.",
+    author="Umar Khan",
+    url="https://github.com/The-Pied-Piper/PyCache",
+    packages=["cache"],
+)
